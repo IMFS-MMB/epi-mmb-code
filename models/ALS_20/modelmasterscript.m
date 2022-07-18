ecb818408210c6afca00409d6f3c92a62e5369ee
KaiLong <32761724+jasperiankl@users.noreply.github.com>
2022-07-18 18:26:03 +0200

    update ALS_20

--- models/ALS_20/modelmasterscript.m
+++ models/ALS_20/modelmasterscript.m
@@ -19,6 +19,12 @@ Susceptibles = nan(size(Output));
 Infected = day2week(uk_sir) ;
 Recovered = nan(size(Output));
 Deaths = day2week(uk_death) ;
+Inflation= NaN(size(Susceptibles));
+Interest= NaN(size(Susceptibles));
+Investment = NaN(size(Susceptibles));
+
+
+
 Consumption_ss = 0;%Consumption(1);
 Labour_ss = Labour(1);
 Output_ss = 0;%Output(1);
@@ -26,5 +32,9 @@ Susceptibles_ss= 0;
 Infected_ss= 0;
 Recovered_ss= 0;
 Deaths_ss= 0;
-save('simulated_results.mat','Consumption','Labour','Output','Deaths','Susceptibles','Infected','Recovered');
-save('simulated_results_ss.mat','Consumption_ss','Labour_ss','Output_ss','Deaths_ss','Susceptibles_ss','Infected_ss','Recovered_ss');%,'Exposed'
+
+Inflation_ss= 0;
+Interest_ss= 0;
+Investment_ss = 0;
+save('simulated_results.mat','Consumption','Labour','Output','Deaths','Susceptibles','Infected','Recovered','Interest','Inflation','Investment');
+save('simulated_results_ss.mat','Consumption_ss','Labour_ss','Output_ss','Deaths_ss','Susceptibles_ss','Infected_ss','Recovered_ss','Interest_ss','Inflation_ss','Investment_ss');%,'Exposed'
