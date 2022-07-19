54777ae7f480f602b3184aabcc03865178763a19
Alexander <57352981+AlexDece@users.noreply.github.com>
2022-07-19 09:35:22 +0200

    adjust AJRT to R Pi Inv

--- models/AJRT_20/Run_Benchmarks.m
+++ models/AJRT_20/Run_Benchmarks.m
@@ -9,6 +9,9 @@ Susceptibles = (S+Sstar)./(pop0+popstar0);
 Infected = (I+Istar)./(pop0+popstar0); 
 Recovered = (R+Rstar)./(pop0+popstar0); 
 Deaths = pop0-Susceptibles-Infected-Recovered;
+Interest=NaN(size(Susceptibles));
+Inflation=NaN(size(Susceptibles));
+Investment=NaN(size(Susceptibles));
 
 Consumption_ss = (pop0+popstar0)*xrss;
 Labour_ss = (pop0+popstar0)*lrss;
@@ -17,9 +20,12 @@ Susceptibles_ss=0;
 Infected_ss=0;
 Recovered_ss=0;
 Deaths_ss=0;
+Interest_ss=0;
+Inflation_ss=0;
+Investment_ss=0;
 
-save('simulated_results.mat','Consumption','Labour','Output','Deaths','Susceptibles','Infected','Recovered');
-save('simulated_results_ss.mat','Consumption_ss','Labour_ss','Output_ss','Deaths_ss','Susceptibles_ss','Infected_ss','Recovered_ss');
+save('simulated_results.mat','Consumption','Labour','Output','Deaths','Susceptibles','Infected','Recovered','Interest','Inflation','Investment');
+save('simulated_results_ss.mat','Consumption_ss','Labour_ss','Output_ss','Deaths_ss','Susceptibles_ss','Infected_ss','Recovered_ss','Interest_ss','Inflation_ss','Investment_ss');
 
 
 %save(['../Mat/', job_name, '_Benchmark.mat']);
